12d8b2863b7b431ebe7d59a653bce0c5ea6db60f
gabe greenfield <gabegreenfield@MacBook-Air.local>
2019-08-21 23:54:18 -0600

    setting up beginning of project and gql

---
+++ plugins/apollo-error-handler.js
@@ -0,0 +1,8 @@
+// plugins/apollo-error-handler.js
+export default (error, context) => {
+    console.log(error)
+    context.error({
+        statusCode: 304,
+        message: 'Server error'
+    })
+}
